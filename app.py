4056e66023b637b73f0754767b02b86fba2e1754
zack <nyagwayaezekiah@gmail.com>
2025-05-30 21:12:04 +0300

    made some changes

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ app = Flask(__name__)
 
 
 # endpoint to ai wildlife ranger
-PING_ARTS_EXPERIENCE = os.getenv('PING_ARTS_EXPERIENCE')
-PING_AI_WILDLIFE_RANGER = os.getenv('PING_AI_WILDLIFE_RANGER')
+PING_ARTS_EXPERIENCE_TATTOO = os.getenv('PING_ARTS_EXPERIENCE_TATTOO')
 
 LOG_FILE = "keep_alive_log.txt"
 
@@ -30,32 +29,17 @@ def ping_servers():
     """Send a request to each app server to keep them alive."""
     while True:
         try:
-            res_arts_experience = requests.get(PING_ARTS_EXPERIENCE, timeout=10)
-            res_ai_wildlife_ranger = requests.get(PING_AI_WILDLIFE_RANGER, timeout=10)
-            if res_arts_experience.status_code == 200 and res_ai_wildlife_ranger.status_code == 200:
-                message_arts = f"Ping to arts-experience was successful. Response: {res_arts_experience.json().get('message')}"
+            res_arts_experience_tattoo = requests.get(PING_ARTS_EXPERIENCE_TATTOO, timeout=10)
+            if res_arts_experience_tattoo.status_code == 200:
+                message_arts = f"Ping to arts-experience-tattoo was successful. Response: {res_arts_experience_tattoo.json().get('message')}"
                 print(message_arts)
                 log_message(message_arts)
 
-                message_ranger = f"Ping to ai-wildlife-ranger was successful. Response: {res_ai_wildlife_ranger.json().get('message')}"
-                print(message_ranger)
-                log_message(message_ranger)
-            elif res_arts_experience.status_code == 200 or res_ai_wildlife_ranger.status_code == 200:
-                message_arts = f"Ping to arts-experience was successful. Response: {res_arts_experience.json().get('message')}"
-                print(message_arts)
-                log_message(message_arts)
-
-                message_ranger = f"Ping to ai-wildlife-ranger was successful. Response: {res_ai_wildlife_ranger.json().get('message')}"
-                print(message_ranger)
-                log_message(message_ranger)
             else:
-                message_arts = f"Either the app server is restarting or is busy: {res_arts_experience.status_code}"
+                message_arts = f"Either the app server is restarting or is busy: {res_arts_experience_tattoo.status_code}"
                 print(message_arts)
                 log_message(message_arts)
 
-                message_ranger = f"Either the app server is restarting or is busy: {res_ai_wildlife_ranger.status_code}"
-                print(message_ranger)
-                log_message(message_ranger)
         except requests.RequestException as e:
             message = f"Failed to reach app.py. Network Issue: {e}"
             log_message(message)
@@ -65,14 +49,15 @@ def ping_servers():
 
 @app.route('/ping', methods=['GET'])
 def handle_ping():
-    """Handle incomig ping requests"""
-    log_message("Received ping from arts-experience: Active")
+    """Handle incoming ping requests"""
+    log_message("Received ping from arts-experience-tattoo: Active")
     return jsonify({"message": "Keep Alive Worker is active"}), 200
 
-# Start the ping app servers function as a separate process
-ping_process = Process(target=ping_servers)
-ping_process.start()
 
 if __name__ == "__main__":
+    # Start the ping app servers function as a separate process
+    ping_process = Process(target=ping_servers)
+    ping_process.start()
+    
     # Start the Flask app
     app.run(debug=False, port=5001)
